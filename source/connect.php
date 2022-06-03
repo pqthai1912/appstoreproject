98c6662046b3194269074e0faa4c1c930974a239
arirator120 <arirator120@gmail.com>
2022-06-03 15:07:02 +0700

    Delete old project

--- source/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli('localhost', 'thaiphan', '123456', 'database');
-mysqli_set_charset($mysqli, 'UTF8');
-// Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
-?>
